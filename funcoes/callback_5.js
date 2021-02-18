c020a16a54f90e2e79c315a37cb89164c59ed7dd
Marcus Aguiar <marcustsu@gmail.com>
2021-02-18 08:10:58 -0300

    aula 28

--- funcoes/callback_5.js
+++ funcoes/callback_5.js
@@ -9,7 +9,7 @@ const carrinho = [
     { name: 'Caderno', amount: 4, price: 27.10, fragile: false },
     { name: 'Lápis', amount: 3, price: 5.82, fragile: false },
     { name: 'Tesoura', amount: 1, price: 19.20, fragile: true },
-];
+]
 
 //Get total of fragile items
 const getTotalOfFragiles = carrinho.filter(item => item.fragile) //only fragile
@@ -20,11 +20,55 @@ console.log(getTotalOfFragiles)
 
 //Get total average
 const totalAverage = getTotalOfFragiles.reduce( (acc, item, index, array) => {
-                                let sum =  acc + item;
+                                let sum =  acc + item
                                 if (index == array.length - 1)
-                                    sum = sum / array.length;
-                                return sum;
+                                    sum = sum / array.length
+                                return sum
                             })
 
-console.log("Total average: ")
-console.log(totalAverage)
+console.log("Total average: ", totalAverage)
+
+/**
+ * Aula 28
+ * Criar o próprio reduce
+ */
+
+ Array.prototype.customReduce =  function (fn, ini) {
+    
+    let acc = this[0]
+    let initialIndex = 1
+
+    if (ini) {
+        acc = ini  
+        initialIndex--
+    }
+
+    for(let i = initialIndex; i < this.length; i++){
+        acc = fn(acc, this[i], i, this)
+    }
+
+    return acc
+ }
+
+ //Test custom reduce
+
+ const totalAverageCustomReduce = getTotalOfFragiles.customReduce( (acc, item, index, array) => {
+    console.log("i: ", index)
+    console.log("acc: ", acc)
+    console.log("item: ", item)
+    console.log("array length: ", array.length)
+    console.log("-------------------")
+    let sum =  acc + item
+    if (index == array.length - 1)
+        sum = sum / array.length
+    return sum
+})
+ console.log("Total average with custom reduce: ", totalAverageCustomReduce)
+
+ //Test 2, sum passing initial value
+
+const numberArray = [1, 2, 3, 4, 5]
+
+const sum = numberArray.reduce((acc, item) => acc + item, 0)
+
+console.log("Sum with custom reduce: ", sum)
